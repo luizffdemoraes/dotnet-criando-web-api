d2192a24914bc6cb3822619690a2e7e198ddded6
Luiz Felipe <lffm1994@gmail.com>
2023-02-26 23:24:41 -0300

    Paginando resultados

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -10,10 +10,12 @@ namespace FilmesApi.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
             Console.WriteLine(filme.Duracao);
@@ -22,9 +24,15 @@ namespace FilmesApi.Controllers
 
         // IEnumerable<Filme>
         [HttpGet]
-        public IEnumerable<Filme> RecuperaFilmes()
+        public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, int take = 50)
         {
-            return filmes;
+            return filmes.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public Filme? RecuperaFilmesPorId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
         }
     }
 }
