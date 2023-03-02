e8664f4d4f7fdf4a41f59c5ac03220639d2bae0a
Luiz Felipe <lffm1994@gmail.com>
2023-03-01 22:40:44 -0300

    Criando a Sessão

--- Data/FilmesContext.cs
+++ Data/FilmesContext.cs
@@ -12,5 +12,6 @@ namespace FilmesApi.Data
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Sessao> Sessoes { get; set; }
     }
 }
