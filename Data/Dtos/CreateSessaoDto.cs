efda15791c1530e773da8447153e431c367da51f
Luiz Felipe <lffm1994@gmail.com>
2023-03-02 20:31:53 -0300

    Relacionando Filme e Sessão

--- Data/Dtos/CreateSessaoDto.cs
+++ Data/Dtos/CreateSessaoDto.cs
@@ -1,6 +1,9 @@
-﻿namespace FilmesApi.Data.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos
 {
     public class CreateSessaoDto
     {
+        public int FilmeId { get; set; }
     }
 }
